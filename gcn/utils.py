ded62defc9cfbc1d09128f208df93d60197e13c4
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2020-02-21 00:29:47 +0800

    try to run with my own data

--- gcn/utils.py
+++ gcn/utils.py
@@ -21,64 +21,49 @@ def sample_mask(idx, l):
     return np.array(mask, dtype=np.bool)
 
 
-def load_data(dataset_str):
-    """
-    Loads input data from gcn/data directory
+def load_data_custom():
 
-    ind.dataset_str.x => the feature vectors of the training instances as scipy.sparse.csr.csr_matrix object;
-    ind.dataset_str.tx => the feature vectors of the test instances as scipy.sparse.csr.csr_matrix object;
-    ind.dataset_str.allx => the feature vectors of both labeled and unlabeled training instances
-        (a superset of ind.dataset_str.x) as scipy.sparse.csr.csr_matrix object;
-    ind.dataset_str.y => the one-hot labels of the labeled training instances as numpy.ndarray object;
-    ind.dataset_str.ty => the one-hot labels of the test instances as numpy.ndarray object;
-    ind.dataset_str.ally => the labels for instances in ind.dataset_str.allx as numpy.ndarray object;
-    ind.dataset_str.graph => a dict in the format {index: [index_of_neighbor_nodes]} as collections.defaultdict
-        object;
-    ind.dataset_str.test.index => the indices of test instances in graph, for the inductive setting as list object.
+    # First we load this data
+    features = pkl.load(
+        open('/home/c/cksash/kdd/node_vectors_joined.pkl', 'rb')
+    )
 
-    All objects above must be saved using python pickle module.
+    features = sp.csr_matrix(features)
 
-    :param dataset_str: Dataset name
-    :return: All data input files loaded (as well the training/test data).
-    """
-    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
-    objects = []
-    for i in range(len(names)):
-        with open("data/ind.{}.{}".format(dataset_str, names[i]), 'rb') as f:
-            if sys.version_info > (3, 0):
-                objects.append(pkl.load(f, encoding='latin1'))
-            else:
-                objects.append(pkl.load(f))
+    adj_mat = pkl.load(
+        open('/home/c/cksash/kdd/joined_adjmatrix.pkl', 'rb')
+    )
 
-    x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset_str))
-    test_idx_range = np.sort(test_idx_reorder)
+    adj_mat = sp.csr_matrix(adj_mat)
 
-    if dataset_str == 'citeseer':
-        # Fix citeseer dataset (there are some isolated nodes in the graph)
-        # Find isolated nodes, add them as zero-vecs into the right position
-        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
-        tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-        tx_extended[test_idx_range-min(test_idx_range), :] = tx
-        tx = tx_extended
-        ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
-        ty_extended[test_idx_range-min(test_idx_range), :] = ty
-        ty = ty_extended
+    labels = pkl.load(
+        open('/home/c/cksash/kdd/all_labels.pkl', 'rb')
+    )
 
-    features = sp.vstack((allx, tx)).tolil()
-    features[test_idx_reorder, :] = features[test_idx_range, :]
-    adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
+    # Now we perform the train-test-val split
+    indices = list(range(10))
+    random.shuffle(indices)
 
-    labels = np.vstack((ally, ty))
-    labels[test_idx_reorder, :] = labels[test_idx_range, :]
+    train_ratio = 0.7
+    val_ratio = 0.1
+    test_ratio = 0.2
 
-    idx_test = test_idx_range.tolist()
-    idx_train = range(len(y))
-    idx_val = range(len(y), len(y)+500)
+    assert(train_ratio + val_ratio + test_ratio == 1.0)
 
-    train_mask = sample_mask(idx_train, labels.shape[0])
-    val_mask = sample_mask(idx_val, labels.shape[0])
-    test_mask = sample_mask(idx_test, labels.shape[0])
+    total_elements = len(indices)
+
+    train_indices = indices[0: int(train_ratio * total_elements)]
+    indices = indices[int(train_ratio * total_elements):]
+
+    val_indices = indices[0: int(val_ratio * total_elements)]
+    indices = indices[int(val_ratio * total_elements):]
+
+    test_indices = indices
+
+    # Initialize the masks
+    train_mask = sample_mask(train_indices, labels.shape[0])
+    val_mask = sample_mask(val_indices, labels.shape[0])
+    test_mask = sample_mask(test_indices, labels.shape[0])
 
     y_train = np.zeros(labels.shape)
     y_val = np.zeros(labels.shape)
@@ -87,7 +72,10 @@ def load_data(dataset_str):
     y_val[val_mask, :] = labels[val_mask, :]
     y_test[test_mask, :] = labels[test_mask, :]
 
-    return adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
+    print(adj_mat.shape)
+    print(features.shape)
+
+    return adj_mat, features, y_train, y_val, y_test, train_mask, val_mask, test_mask
 
 
 def sparse_to_tuple(sparse_mx):
